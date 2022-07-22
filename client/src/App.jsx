9f1c3d80da4477221ea4eaa14de595c259d57a1b
Gleb Manov <glebmanov@gmail.com>
2022-07-22 22:37:13 +0300

    added header main tags

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import Weather from '../src/containers/Weather';
-import Header from './containers/Header';
+import React from 'react'
+import Weather from '../src/containers/Weather'
+import Header from './containers/Header'
 
-import './styles/app.scss';
+import './styles/app.scss'
 
 const App = () => {
   return (
     <div className='container'>
-      <Header />
-      <Weather />
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Weather />
+      </main>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
